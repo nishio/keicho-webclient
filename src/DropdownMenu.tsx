3b7e4eeaf6007db6b6b49a0b83a3c22ea7694793
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-01-21 14:00:45 +0900

    add menu

--- src/DropdownMenu.tsx
+++ src/DropdownMenu.tsx
@@ -1,20 +1,58 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import { Menu, MenuItem } from "@material-ui/core";
+
+const exportForRegroup = () => {
+  alert("ok");
+};
 
 export const DropdownMenu = () => {
-  const onClickMenu = () => {
-    alert("no menu yet");
-  };
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
+  const onClickMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (event: React.MouseEvent<HTMLLIElement>) => {
+    setAnchorEl(null);
+  };
+  const MyMenuItem = (props: any) => {
+    const { children, onClick, ...other } = props;
+    const f = () => {
+      onClick();
+      setAnchorEl(null);
+    };
+    return (
+      <MenuItem onClick={f} {...other}>
+        {children}
+      </MenuItem>
+    );
+  };
   return (
-    <IconButton
-      edge="end"
-      color="inherit"
-      aria-label="menu"
-      onClick={onClickMenu}
-    >
-      <MenuIcon />
-    </IconButton>
+    <>
+      <IconButton
+        edge="end"
+        color="inherit"
+        aria-label="menu"
+        onClick={onClickMenu}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        // keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MyMenuItem onClick={exportForRegroup}>Export for Regroup</MyMenuItem>
+        <MyMenuItem onClick={handleClose} disabled>
+          Export for Scrapbox
+        </MyMenuItem>
+        <MyMenuItem onClick={handleClose} disabled>
+          Export as Text
+        </MyMenuItem>
+      </Menu>
+    </>
   );
 };
