679c2676b8efe0ee6f553e27e0ca64a671e8e328
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-01-26 14:53:12 +0900

    show question and selected keywords

--- src/exportForRegroup.tsx
+++ src/exportForRegroup.tsx
@@ -3,21 +3,27 @@ import { talkObject } from "./ShowLog";
 
 export const exportForRegroup = () => {
   const lines: string[] = [];
+  const litsk: { [key: number]: string[] } = {};
+  if (talkObject.line_id_to_selected_keywords) {
+    talkObject.line_id_to_selected_keywords.forEach((x: [number, string[]]) => {
+      litsk[x[0]] = x[1];
+    });
+  }
+
   talkObject.log.forEach((x: any, i: number) => {
-    console.log(i, x);
     if (x[0]) {
       // is user
       lines.push(x[1]); // x.text
-    }
-  });
-  const keywords: Set<string> = new Set();
-  talkObject.used_qid_and_words.forEach((x: any, i: number) => {
-    if (x.length >= 2) {
-      x[1].forEach((x: string) => {
-        keywords.add(x);
-      });
+      lines.push(""); // blankline
+    } else {
+      lines.push(x[1]); // x.text
+      if (litsk[i]) {
+        litsk[i].forEach((kw) => {
+          lines.push(kw); // selected keywords
+        });
+      }
     }
   });
 
-  openRegroupDialog(lines.concat(Array.from(keywords)));
+  openRegroupDialog(lines);
 };
