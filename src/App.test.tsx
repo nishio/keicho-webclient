95864d13282703890006e20437a9425099162a50
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-02-15 14:58:05 +0900

    add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import { initializeGlobalState } from "./initializeGlobalState";
+import pretty from "pretty";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import * as getNewTalkID from "./getNewTalkID";
+jest.spyOn(getNewTalkID, "getNewTalkID").mockImplementation(() => {
+  getNewTalkID._gotNewTalkID("test");
+});
+
+let container: HTMLDivElement;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  initializeGlobalState();
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+});
+
+test("render", () => {
+  act(() => {
+    render(<App />, container);
+  });
+  expect(pretty(container.innerHTML)).toMatchSnapshot();
 });
