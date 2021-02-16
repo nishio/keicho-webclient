de3d01146c2813a1f9ab8040b5a5f488e368cb56
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-02-16 18:33:33 +0900

    add ShowLog test, temporary stop NewTalk test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import App from "./App";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { unmountComponentAtNode } from "react-dom";
 import { initializeGlobalState } from "./initializeGlobalState";
-import pretty from "pretty";
-import ReactTestUtils from "react-dom/test-utils";
 
 import * as getNewTalkID from "./getNewTalkID";
-import * as sendToServer from "./sendToServer";
+
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import * as loadLogsModule from "./loadLogsFromFirestore";
+import * as MockTalkObject from "./talkObject.json";
+import { ShowLog } from "./ShowLog";
+import * as RegroupDialogModule from "./RegroupDialog";
+
 jest.spyOn(getNewTalkID, "getNewTalkID").mockImplementation(() => {
   getNewTalkID._gotNewTalkID("test");
 });
@@ -20,7 +28,9 @@ beforeEach(() => {
   // setup a DOM element as a render target
   container = document.createElement("div");
   document.body.appendChild(container);
-  initializeGlobalState();
+  act(() => {
+    initializeGlobalState();
+  });
 });
 
 afterEach(() => {
@@ -30,37 +40,77 @@ afterEach(() => {
 });
 
 test("render", () => {
-  act(() => {
-    render(<App />, container);
-  });
-  expect(pretty(container.innerHTML)).toMatchSnapshot();
-
-  jest.spyOn(sendToServer, "sendToServer").mockImplementation(() => {
-    sendToServer._gotResponse([{ user: true, text: "aaa" }], {
-      text: "bbb",
-      last_kw: ["a"],
-      other_kw: ["b"],
-    });
-  });
-
-  const textarea = container.querySelector("[id=textarea]");
-  expect(textarea).not.toBeNull();
-
-  act(() => {
-    textarea?.dispatchEvent(
-      new KeyboardEvent("keypress", {
-        key: "Enter",
-        bubbles: true,
-      })
-    );
-    const e = textarea as HTMLTextAreaElement;
-    e.value = "hello";
-    // ReactTestUtils.Simulate.keyPress(e, {
-    //   key: "Space",
-    // });
-    ReactTestUtils.Simulate.keyPress(e, {
-      key: "Enter",
-    });
-  });
-  expect(pretty(container.innerHTML)).toMatchSnapshot("reply");
+  // act(() => {
+  //   render(<App />, container);
+  // });
+  // expect(pretty(container.innerHTML)).toMatchSnapshot();
+  // jest.spyOn(sendToServer, "sendToServer").mockImplementation(() => {
+  //   sendToServer._gotResponse([{ user: true, text: "aaa" }], {
+  //     text: "bbb",
+  //     last_kw: ["a"],
+  //     other_kw: ["b"],
+  //   });
+  // });
+  // const textarea = container.querySelector("[id=textarea]");
+  // expect(textarea).not.toBeNull();
+  // act(() => {
+  //   textarea?.dispatchEvent(
+  //     new KeyboardEvent("keypress", {
+  //       key: "Enter",
+  //       bubbles: true,
+  //     })
+  //   );
+  //   const e = textarea as HTMLTextAreaElement;
+  //   e.value = "hello";
+  //   // ReactTestUtils.Simulate.keyPress(e, {
+  //   //   key: "Space",
+  //   // });
+  //   ReactTestUtils.Simulate.keyPress(e, {
+  //     key: "Enter",
+  //   });
+  // });
+  // expect(pretty(container.innerHTML)).toMatchSnapshot("reply");
 });
+
+test("show log", async () => {
+  const m = jest
+    .spyOn(loadLogsModule, "loadLogsFromFirestore")
+    .mockResolvedValue(MockTalkObject);
+
+  // no talkID needed beacuse loadLogsFromFirestore is mocked
+  render(<ShowLog talk="" />);
+  await waitFor(() => screen.getByText("🙁"));
+
+  const m2 = jest.spyOn(RegroupDialogModule, "openRegroupDialog");
+  fireEvent.click(screen.getByLabelText("menu"));
+  fireEvent.click(screen.getByText("Export for Regroup"));
+  expect(m2).toHaveBeenCalled();
+  expect(m2.mock.calls[0][0]).toMatchSnapshot();
+
+  m.mockRestore();
+  m2.mockRestore();
+});
+
+// expect(
+//   screen.getByTestId("textarea-export-for-regroup").nodeValue
+// ).toMatchSnapshot();
+
+// const m = jest.spyOn(reactn, "setGlobal");
+// // render(<ShowLog talk="SJSLzd0PCLcJ3Nzlfdc4" />);
+// await loadLogs("SJSLzd0PCLcJ3Nzlfdc4");
+// expect(m.mock.calls).toMatchSnapshot();
+// const m = jest.spyOn(RegroupDialog, "openRegroupDialog");
+// // expect(pretty(container.innerHTML)).toMatchSnapshot();
+// fireEvent.click(await screen.findByLabelText("menu"));
+// fireEvent.click(await screen.findByText("Export for Regroup"));
+// await expect(m).toHaveBeenCalled();
+// m.mockRestore();
+// act(() => {
+//   ReactTestUtils.Simulate.click(container.querySelector("[id=menu]")!);
+// });
+// act(() => {
+//   ReactTestUtils.Simulate.click(
+//     container.querySelector("[id=exportForScrapbox]")!
+//   );
+// });
+// expect(pretty(container.innerHTML)).toMatchSnapshot();
