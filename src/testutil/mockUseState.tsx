13c98a4c727734a3f64635d31629698df3c8ed2b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-03-15 17:21:10 +0900

    update

--- src/testutil/mockUseState.tsx
+++ src/testutil/mockUseState.tsx
@@ -1,20 +1,21 @@
-import React, { Dispatch } from "react";
+import React, { Dispatch, useCallback } from "react";
 import { act } from "@testing-library/react";
 import { useState as originalUseState } from "react";
 
 export const mockUseState = () => {
-  jest.spyOn(React, "useState").mockImplementation((arg?: unknown): [
+  return jest.spyOn(React, "useState").mockImplementation((arg?: unknown): [
     unknown,
     Dispatch<unknown>
   ] => {
-    const [s, setS] = originalUseState(arg);
-    return [
-      s,
-      (arg: unknown) => {
+    const [s, dispatch] = originalUseState(arg);
+    const wrappedDispatch = useCallback(
+      (arg: unknown): void => {
         act(() => {
-          setS(arg);
+          dispatch(arg);
         });
       },
-    ];
+      [dispatch]
+    );
+    return [s, wrappedDispatch];
   });
 };
