f1558977ab567d79bca2ffa353c76975e577ae4c
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-03-22 13:00:20 +0900

    update

--- src/DropdownMenuForNewTalk.tsx
+++ src/DropdownMenuForNewTalk.tsx
@@ -5,6 +5,7 @@ import { ShowLastTalk } from "./ShowLastTalk";
 import { DropdownMenuCommon } from "./DropdownMenuCommon";
 import { AutoCloseMenuItem } from "./AutoCloseMenuItem";
 import * as Sentry from "@sentry/browser";
+import { suppressDialog } from "./initSentry";
 
 export const DropdownMenuForNewTalk = () => {
   return (
@@ -26,5 +27,13 @@ export const DropdownMenuForNewTalk = () => {
 };
 
 const forceError = () => {
-  Sentry.captureMessage("FORCED_ERROR");
+  // Sentry.captureMessage("FORCED_ERROR");
+  Promise.resolve(0)
+    .then((x) => {
+      throw new TypeError("Manual Error");
+    })
+    .catch((e) => {
+      suppressDialog();
+      Sentry.captureException(e);
+    });
 };
