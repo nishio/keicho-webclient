529635f115e397af3c146e9065cc59c8eefb5608
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-03-22 12:59:38 +0900

    can suppress Dialog

--- src/index.tsx
+++ src/index.tsx
@@ -5,29 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { initializeGlobalState } from "./initializeGlobalState";
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import { initSentry } from "./initSentry";
 
-Sentry.init({
-  dsn:
-    "https://e4908ccb65fa4e7cb63ff1e84b55ba1f@o376998.ingest.sentry.io/5627136",
-  integrations: [new Integrations.BrowserTracing()],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV,
-  beforeSend(event, hint) {
-    // Check if it is an exception, and if so, show the report dialog
-    console.log(event, hint);
-    if (event.exception && !shownReportDialog) {
-      shownReportDialog = true;
-      Sentry.showReportDialog({ eventId: event.event_id });
-    }
-    return event;
-  },
-});
-
-let shownReportDialog = false;
+initSentry();
 initializeGlobalState();
 ReactDOM.render(
   // <React.StrictMode>
---
+++ src/initSentry.tsx
@@ -0,0 +1,37 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+let shownReportDialog = false;
+let _suppressDialog = false;
+
+export const suppressDialog = () => {
+  _suppressDialog = true;
+};
+
+export const initSentry = () => {
+  Sentry.init({
+    dsn:
+      "https://e4908ccb65fa4e7cb63ff1e84b55ba1f@o376998.ingest.sentry.io/5627136",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+    environment: process.env.NODE_ENV,
+    beforeSend(event, hint) {
+      // Check if it is an exception, and if so, show the report dialog
+
+      if (event.exception) {
+        if (_suppressDialog) {
+          _suppressDialog = false;
+          return event;
+        }
+        if (!shownReportDialog) {
+          shownReportDialog = true;
+          Sentry.showReportDialog({ eventId: event.event_id });
+        }
+      }
+      return event;
+    },
+  });
+};
