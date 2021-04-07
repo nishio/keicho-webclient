a7a4c3a8a101a3cf3dda79e3d7a81650954996c2
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-04-07 18:07:30 +0900

    change local server address

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import { NewTalk } from "./NewTalk";
 import { ShowLog } from "./ShowLog";
-import { USE_LOCAL_SERVER } from "./USE_LOCAL_SERVER";
+import { LOCAL_SERVER, USE_LOCAL_SERVER } from "./USE_LOCAL_SERVER";
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 export const APIROOT =
   IS_PRODUCTION || !USE_LOCAL_SERVER
     ? "https://keicho.herokuapp.com/api/"
-    : "http://localhost:5000/api/";
+    : LOCAL_SERVER;
 
 const getTalkID = (): string => {
   const hash = new URLSearchParams(window.location.hash.substring(1));
