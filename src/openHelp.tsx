bff7a96640f4c373859171df9c56795cdf822863
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-09-10 15:44:51 +0900

    change help url

--- src/openHelp.tsx
+++ src/openHelp.tsx
@@ -1,6 +1,7 @@
 export const openHelp = () => {
+  // https://scrapbox.io/nishio/かんがえをひきだすチャットボットKeichobot
   window.open(
-    "https://scrapbox.io/nishio/%E8%81%9E%E3%81%8D%E5%87%BA%E3%81%97%E3%83%81%E3%83%A3%E3%83%83%E3%83%88%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0",
+    "https://scrapbox.io/nishio/%E3%81%8B%E3%82%93%E3%81%8C%E3%81%88%E3%82%92%E3%81%B2%E3%81%8D%E3%81%A0%E3%81%99%E3%83%81%E3%83%A3%E3%83%83%E3%83%88%E3%83%9C%E3%83%83%E3%83%88Keichobot",
     "_blank"
   );
 };
